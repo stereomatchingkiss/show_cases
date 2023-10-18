413bb60ce1ed2a7fa0e154cb2bed6ff3014adc23
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-18 10:41:11 +0800

    implement opencv frame capture 1.0

--- libs/multimedia/camera/frame_capture_opencv_worker.cpp
+++ libs/multimedia/camera/frame_capture_opencv_worker.cpp
@@ -1,6 +1,69 @@
 #include "frame_capture_opencv_worker.hpp"
 
-frame_capture_opencv_worker::frame_capture_opencv_worker()
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+#include <QDebug>
+
+#include <chrono>
+#include <thread>
+
+#include "frame_capture_params.hpp"
+
+namespace ocv{
+
+struct frame_capture_opencv_worker::impl
 {
+    impl(frame_capture_params params) :
+        params_(std::move(params))
+    {
+
+    }
+
+    bool open_cam() noexcept
+    {
+        try{
+            return cap_.open(std::stoi(params_.url_));
+        }catch(std::exception const &){
+            return cap_.open(params_.url_);
+        }
+    }
+
+    cv::VideoCapture cap_;
+    frame_capture_params params_;
+    std::atomic<bool> stop_ = false;
+};
+
+frame_capture_opencv_worker::frame_capture_opencv_worker(frame_capture_params params, QObject *parent) :
+    frame_capture_base_worker({}, parent),
+    impl_(std::make_unique<impl>(std::move(params)))
+{
+
+}
+
+frame_capture_opencv_worker::~frame_capture_opencv_worker()
+{
+
+}
+
+void frame_capture_opencv_worker::start()
+{
+    if(impl_->open_cam()){
+        impl_->stop_ = true;
+        auto const duration = std::chrono::milliseconds(1000/impl_->params_.max_fps_);
+        while(!impl_->stop_){
+            cv::Mat frame;
+            if(impl_->cap_>>frame; !frame.empty()){
+                call_listeners(frame);
+                std::this_thread::sleep_for(duration);
+            }
+        }
+    }
+}
+
+void frame_capture_opencv_worker::stop()
+{
+    impl_->stop_ = true;
+}
 
 }
--- libs/multimedia/camera/frame_capture_opencv_worker.hpp
+++ libs/multimedia/camera/frame_capture_opencv_worker.hpp
@@ -1,11 +1,31 @@
 #ifndef FRAME_CAPTURE_OPENCV_HPP
 #define FRAME_CAPTURE_OPENCV_HPP
 
+#include "frame_capture_base_worker.hpp"
 
-class frame_capture_opencv
+#include <memory>
+
+namespace ocv{
+
+struct frame_capture_params;
+
+class frame_capture_opencv_worker : public frame_capture_base_worker
 {
+    Q_OBJECT
 public:
-    frame_capture_opencv();
+    explicit frame_capture_opencv_worker(frame_capture_params params, QObject *parent = nullptr);
+    ~frame_capture_opencv_worker();
+
+    virtual void stop() override;
+
+public slots:
+    virtual void start() override;    
+
+private:
+    struct impl;
+    std::unique_ptr<impl> impl_;
 };
+
+}
 
 #endif // FRAME_CAPTURE_OPENCV_HPP
