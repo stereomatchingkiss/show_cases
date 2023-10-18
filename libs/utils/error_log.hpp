f2010d3a4677b848f4f022ee3507e576f110b803
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-18 16:10:24 +0800

    first commit

---
+++ libs/utils/error_log.hpp
@@ -0,0 +1,18 @@
+#ifndef ERROR_LOG_HPP
+#define ERROR_LOG_HPP
+
+#include <format>
+#include <source_location>
+#include <string>
+
+namespace ocv{
+
+inline
+std::string create_error_msg(std::source_location const &loc, std::string const &msg)
+{
+    return std::format("{}, {}, {}\n{}", loc.file_name(), loc.function_name(), loc.line(), msg);
+}
+
+}
+
+#endif // ERROR_LOG_HPP
---
+++ libs/utils/source_type.hpp
@@ -0,0 +1,15 @@
+#ifndef SOURCE_TYPE_HPP
+#define SOURCE_TYPE_HPP
+
+namespace ocv{
+
+enum class stream_source_type : int
+{
+    webcam,
+    video,
+    rtsp
+};
+
+}
+
+#endif // SOURCE_TYPE_HPP
