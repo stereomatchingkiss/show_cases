358f4dc31c416465a5289a532ee6ab5a586f08af
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-20 07:19:46 +0800

    easier to use

--- libs/multimedia/camera/frame_capture_params.hpp
+++ libs/multimedia/camera/frame_capture_params.hpp
@@ -1,6 +1,8 @@
 #ifndef FRAME_CAPTURE_PARAMS_HPP
 #define FRAME_CAPTURE_PARAMS_HPP
 
+#include "../../utils/source_type.hpp"
+
 #include <string>
 
 namespace ocv{
@@ -8,7 +10,8 @@ namespace ocv{
 struct frame_capture_params
 {
     int max_fps_ = 30;
-    std::string url_;
+    stream_source_type src_type_;
+    std::string url_;    
 };
 
 }
