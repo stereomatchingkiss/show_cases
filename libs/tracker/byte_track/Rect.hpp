e34bbfb7646920dc97d15caf6088c1adac45c506
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-23 12:45:07 +0800

    move template class and function to header file

--- libs/tracker/byte_track/Rect.hpp
+++ libs/tracker/byte_track/Rect.hpp
@@ -17,39 +17,126 @@ using Xyah = Eigen::Matrix<T, 1, 4, Eigen::RowMajor>;
 template<typename T>
 class Rect
 {
-    public:
+public:
     Tlwh<T> tlwh;
 
     Rect() = default;
-    Rect(const T &x, const T &y, const T &width, const T &height);
+    Rect(const T &x, const T &y, const T &width, const T &height) :
+        tlwh({x, y, width, height})
+    {
+    }
 
-    ~Rect();
+    T x() const
+    {
+        return tlwh[0];
+    }
 
-    const T &x() const;
-    const T &y() const;
-    const T &width() const;
-    const T &height() const;
+    T y() const
+    {
+        return tlwh[1];
+    }
 
-    T &x();
-    T &y();
-    T &width();
-    T &height();
+    T width() const
+    {
+        return tlwh[2];
+    }
 
-    const T &tl_x() const;
-    const T &tl_y() const;
-    T br_x() const;
-    T br_y() const;
+    T height() const
+    {
+        return tlwh[3];
+    }
 
-    Tlbr<T> getTlbr() const;
-    Xyah<T> getXyah() const;
+    T& x()
+    {
+        return tlwh[0];
+    }
 
-    float calcIoU(const Rect<T>& other) const;
+    T& y()
+    {
+        return tlwh[1];
+    }
+
+    T& width()
+    {
+        return tlwh[2];
+    }
+
+    T& height()
+    {
+        return tlwh[3];
+    }
+
+    T tl_x() const
+    {
+        return tlwh[0];
+    }
+
+    T tl_y() const
+    {
+        return tlwh[1];
+    }
+
+    T br_x() const
+    {
+        return tlwh[0] + tlwh[2];
+    }
+
+    T br_y() const
+    {
+        return tlwh[1] + tlwh[3];
+    }
+
+    Tlbr<T> getTlbr() const
+    {
+        return {
+            tlwh[0],
+            tlwh[1],
+            tlwh[0] + tlwh[2],
+            tlwh[1] + tlwh[3],
+        };
+    }
+
+    Xyah<T> getXyah() const
+    {
+        return {
+            tlwh[0] + tlwh[2] / 2,
+            tlwh[1] + tlwh[3] / 2,
+            tlwh[2] / tlwh[3],
+            tlwh[3],
+        };
+    }
+
+    float calcIoU(Rect<T> const &other) const
+    {
+        float box_area = (other.tlwh[2] + 1) * (other.tlwh[3] + 1);
+        float iw = std::min(tlwh[0] + tlwh[2], other.tlwh[0] + other.tlwh[2]) - std::max(tlwh[0], other.tlwh[0]) + 1;
+        float iou = 0;
+        if (iw > 0)
+        {
+            float ih = std::min(tlwh[1] + tlwh[3], other.tlwh[1] + other.tlwh[3]) - std::max(tlwh[1], other.tlwh[1]) + 1;
+            if (ih > 0)
+            {
+                float ua = (tlwh[0] + tlwh[2] - tlwh[0] + 1) * (tlwh[1] + tlwh[3] - tlwh[1] + 1) + box_area - iw * ih;
+                iou = iw * ih / ua;
+            }
+        }
+        return iou;
+    }
+
+
 };
 
 template<typename T>
-Rect<T> generate_rect_by_tlbr(const Tlbr<T>& tlbr);
+Rect<T> generate_rect_by_tlbr(const byte_track::Tlbr<T>& tlbr)
+{
+    return byte_track::Rect<T>(tlbr[0], tlbr[1], tlbr[2] - tlbr[0], tlbr[3] - tlbr[1]);
+}
 
 template<typename T>
-Rect<T> generate_rect_by_xyah(const Xyah<T>& xyah);
+Rect<T> generate_rect_by_xyah(const byte_track::Xyah<T>& xyah)
+{
+    const auto width = xyah[2] * xyah[3];
+    return byte_track::Rect<T>(xyah[0] - width / 2, xyah[1] - xyah[3] / 2, width, xyah[3]);
+}
 
 }
