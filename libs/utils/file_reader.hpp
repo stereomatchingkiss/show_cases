fef76864a298fd246055f8bd8998a74b013661dc
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-24 13:38:46 +0800

    first commit

---
+++ libs/utils/file_reader.hpp
@@ -0,0 +1,13 @@
+#ifndef FILE_READERS_HPP
+#define FILE_READERS_HPP
+
+#include <string>
+#include <vector>
+
+namespace ocv{
+
+std::vector<std::string> read_file_per_lines(std::string const &fname);
+
+}
+
+#endif // FILE_READERS_HPP
