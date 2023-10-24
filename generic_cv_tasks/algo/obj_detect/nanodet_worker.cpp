886c86740023aa957d6c07242bb480a9d94169ce
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-24 13:39:45 +0800

    add tracking function

--- generic_cv_tasks/algo/obj_detect/nanodet_worker.cpp
+++ generic_cv_tasks/algo/obj_detect/nanodet_worker.cpp
@@ -1,11 +1,15 @@
 #include "nanodet_worker.hpp"
 
+#include <cv_algo/converter/box_type_converter.hpp>
 #include <cv_algo/obj_detect/nanodet/nanodet.hpp>
 #include <cv_algo/obj_detect/nanodet/nanodet_utils.hpp>
+#include <cv_algo/tracker/byte_track/BYTETracker.hpp>
+#include <utils/file_reader.hpp>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 
+#include <QDebug>
 #include <QPixmap>
 
 #include <format>
@@ -16,21 +20,18 @@ struct nanodet_worker::impl
     impl(float score_threshold,
          float nms_threshold,
          int input_size)
-        : net_(std::format("assets/obj_detect/nanodet-plus-m_{}.param", input_size).c_str(),
+        : names_{ocv::read_file_per_lines("assets/obj_detect/coco.names")},
+          net_(std::format("assets/obj_detect/nanodet-plus-m_{}.param", input_size).c_str(),
                std::format("assets/obj_detect/nanodet-plus-m_{}.bin", input_size).c_str(), 80, false, false, input_size),
           input_size_{input_size},
           nms_threshold_{nms_threshold},
           score_threshold_{score_threshold}
     {
-        std::ifstream in("assets/obj_detect/coco.names");
-        std::string line;
-        while(std::getline(in, line)){
-            names_.emplace_back(std::move(line));
-        }
     }
 
-    ocv::nanodet net_;
     std::vector<std::string> names_;
+    ocv::nanodet net_;
+    ocv::byte_track::BYTETracker tracker_;
 
     int input_size_;
     float nms_threshold_;
@@ -61,8 +62,19 @@ void nanodet_worker::process_results(std::any frame)
         cv::cvtColor(mat, mat, cv::COLOR_GRAY2RGB);
     }
 
-    auto const det_results = impl_->net_.predict(mat, impl_->score_threshold_, impl_->nms_threshold_);
-    ocv::draw_bboxes(mat, det_results, impl_->names_);
+    auto det_results = impl_->net_.predict(mat, impl_->score_threshold_, impl_->nms_threshold_);
+    const auto [first, last] = std::ranges::remove_if(det_results, [](auto const &val)
+    {
+        return val.label_ != 2;
+    });
+    det_results.erase(first, last);
+    auto track_obj = ocv::box_info_to_byte_track_obj(det_results);
+    auto const track_ptr_vec = impl_->tracker_.update(track_obj);
+
+    det_results = ocv::byte_track_obj_to_box_info(track_ptr_vec, 2);
+    for(auto const &val : det_results){
+        ocv::draw_bboxes_custom(mat, val, std::format("{}:{}", impl_->names_[2], val.track_id_));
+    }
 
     auto qimg = QImage(mat.data, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
     emit send_process_results(QPixmap::fromImage(std::move(qimg)));
