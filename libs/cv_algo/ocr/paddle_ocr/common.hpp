b94a89fe0a301082f95a6421dc05e8d13e2ef37e
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-26 22:06:09 +0800

    first commit

---
+++ libs/cv_algo/ocr/paddle_ocr/common.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+namespace ocv::ocr {
+
+struct TextBox {
+    std::vector<cv::Point> boxPoint;
+    float score;
+    std::string text;
+};
+struct TextLine {
+    std::string text;
+    std::vector<float> charScores;
+};
+
+std::vector<cv::Point> getMinBoxes(const std::vector<cv::Point>& inVec, float& minSideLen, float& allEdgeSize);
+float boxScoreFast(const cv::Mat& inMat, const std::vector<cv::Point>& inBox);
+std::vector<cv::Point> unClip(const std::vector<cv::Point>& inBox, float perimeter, float unClipRatio);
+cv::Mat getRotateCropImage(const cv::Mat& src, std::vector<cv::Point> box);
+std::vector<cv::Mat> getPartImages(const cv::Mat& src, std::vector<TextBox>& textBoxes);
+cv::Mat matRotateClockWise180(cv::Mat src);
+cv::Mat makePadding(cv::Mat& src, const int padding);
+
+}
