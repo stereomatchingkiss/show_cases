2054527dba724aa91723d0b17a381c457cf7e7f5
stereomatchingkiss <thamngapwei@gmail.com>
2023-10-28 10:18:16 +0800

    first commit

---
+++ generic_cv_tasks/global/global_keywords.cpp
@@ -0,0 +1,6 @@
+#include "global_keywords.hpp"
+
+const QString global_keywords::generic_cv_tasks_assets() const
+{
+    return "GenericCVTasksAssets";
+}
---
+++ generic_cv_tasks/global/global_keywords.hpp
@@ -0,0 +1,12 @@
+#ifndef GLOBAL_KEYWORDS_HPP
+#define GLOBAL_KEYWORDS_HPP
+
+#include <QString>
+
+class global_keywords
+{
+public:
+    QString const generic_cv_tasks_assets() const;
+};
+
+#endif // GLOBAL_KEYWORDS_HPP
