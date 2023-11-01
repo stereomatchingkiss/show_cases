6f4f942ebf4cc55cf35824386ed205e29aef7992
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-01 13:10:38 +0800

    add new keywords

--- generic_cv_tasks/global/global_keywords.cpp
+++ generic_cv_tasks/global/global_keywords.cpp
@@ -7,11 +7,6 @@ const QString global_keywords::generic_cv_tasks_assets() const
     return "GenericCVTasksAssets";
 }
 
-const QString global_keywords::label_select_roi_path() const
-{
-    return generic_cv_tasks_assets() + "/label_select_roi.json";
-}
-
 const QString global_keywords::coco_names_path() const
 {
     return "assets/obj_detect/coco.names";
@@ -20,4 +15,24 @@ const QString global_keywords::coco_names_path() const
 std::vector<std::string> global_keywords::coco_names() const
 {
     return flt::read_file_per_lines("assets/obj_detect/coco.names");
+}
+
+QString global_keywords::state_roi() const
+{
+    return "state_roi";
+}
+
+QString global_keywords::state_widget_object_detect_model_select() const
+{
+    return "state_widget_object_detect_model_select";
+}
+
+QString global_keywords::state_widget_select_object_to_detect() const
+{
+    return "state_widget_select_object_to_detect";
+}
+
+QString global_keywords::state_widget_source_selection() const
+{
+    return "state_widget_source_selection";
 }
--- generic_cv_tasks/global/global_keywords.hpp
+++ generic_cv_tasks/global/global_keywords.hpp
@@ -9,11 +9,15 @@
 class global_keywords
 {
 public:
-    QString const generic_cv_tasks_assets() const;
-    QString const label_select_roi_path() const;
+    QString const generic_cv_tasks_assets() const;    
     QString const coco_names_path() const;
 
     std::vector<std::string> coco_names() const;
+
+    QString state_roi() const;
+    QString state_widget_object_detect_model_select() const;
+    QString state_widget_select_object_to_detect() const;
+    QString state_widget_source_selection() const;
 };
 
 #endif // GLOBAL_KEYWORDS_HPP
