c28621e5ce7f2ba0816e13d991bda1377dfe7f9c
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-01 13:11:23 +0800

    first commit

---
+++ generic_cv_tasks/config/config_read_write.cpp
@@ -0,0 +1,42 @@
+#include "config_read_write.hpp"
+
+#include "../global/global_keywords.hpp"
+
+#include <json/json_utils.hpp>
+
+void config_read_write::set_roi(QJsonObject val)
+{
+    roi_ = std::move(val);
+}
+
+void config_read_write::set_widget_object_detect_model_select(QJsonObject val)
+{
+    widget_object_detect_model_select_ = std::move(val);
+}
+
+void config_read_write::set_widget_select_object_to_detect(QJsonObject val)
+{
+    widget_select_object_to_detect_ = std::move(val);
+}
+
+void config_read_write::set_widget_source_selection(QJsonObject val)
+{
+    widget_source_selection_ = std::move(val);
+}
+
+QJsonObject config_read_write::read(const QString &val)
+{
+    return flt::json::parse_file_to_jobj(val);
+}
+
+void config_read_write::write(const QString &val)
+{
+    QJsonObject obj;
+    global_keywords gk;
+    obj[gk.state_roi()] = roi_;
+    obj[gk.state_widget_object_detect_model_select()] = widget_object_detect_model_select_;
+    obj[gk.state_widget_select_object_to_detect()] = widget_select_object_to_detect_;
+    obj[gk.state_widget_source_selection()] = widget_source_selection_;
+
+    flt::json::write_file_to_json(obj, val);
+}
---
+++ generic_cv_tasks/config/config_read_write.hpp
@@ -0,0 +1,24 @@
+#ifndef CONFIG_READ_WRITE_HPP
+#define CONFIG_READ_WRITE_HPP
+
+#include <QJsonObject>
+
+class config_read_write
+{
+public:
+    void set_roi(QJsonObject val);
+    void set_widget_object_detect_model_select(QJsonObject val);
+    void set_widget_select_object_to_detect(QJsonObject val);
+    void set_widget_source_selection(QJsonObject val);
+
+    QJsonObject read(QString const &val);
+    void write(QString const &val);
+
+private:
+    QJsonObject roi_;
+    QJsonObject widget_object_detect_model_select_;
+    QJsonObject widget_select_object_to_detect_;
+    QJsonObject widget_source_selection_;
+};
+
+#endif // CONFIG_READ_WRITE_HPP
