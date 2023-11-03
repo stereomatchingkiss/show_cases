1396fc518c872437cfc8e62619054f57bd9510a5
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-03 17:39:52 +0800

    fix bug, alwasy read the same file

--- libs/utils/file_reader.cpp
+++ libs/utils/file_reader.cpp
@@ -7,7 +7,7 @@ namespace flt{
 std::vector<std::string> read_file_per_lines(const std::string &fname)
 {
     std::vector<std::string> names;
-    std::ifstream in("assets/obj_detect/coco.names");
+    std::ifstream in(fname);
     std::string line;
     while(std::getline(in, line)){
         names.emplace_back(std::move(line));
