f6bd47a05ce55dba58adba6b3b8c9d6e933084a4
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-04 13:20:43 +0800

    do not load name from file, easier to work with wasm

--- generic_cv_tasks/global/global_keywords.cpp
+++ generic_cv_tasks/global/global_keywords.cpp
@@ -24,7 +24,21 @@ const QString global_keywords::coco_names_path() const
 
 std::vector<std::string> global_keywords::coco_names() const
 {
-    return flt::read_file_per_lines("assets/obj_detect/coco.names");
+    return {
+        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train",
+        "truck", "boat", "traffic light", "fire hydrant", "stop sign",
+        "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep",
+        "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+        "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+        "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+        "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork",
+        "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange",
+        "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+        "couch", "potted plant", "bed", "dining table", "toilet", "tv",
+        "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+        "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+        "scissors", "teddy bear", "hair drier", "toothbrush"
+    };
 }
 
 QString global_keywords::state_roi() const
