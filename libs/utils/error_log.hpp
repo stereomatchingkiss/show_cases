7e84fd6eda13ca0f1edb31ae2f6229f833c50d8e
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-05 19:03:13 +0800

    add new function, more versatile

--- libs/utils/error_log.hpp
+++ libs/utils/error_log.hpp
@@ -7,10 +7,14 @@
 
 namespace flt{
 
-inline
-std::string create_error_msg(std::source_location const &loc, std::string const &msg)
+inline std::string create_error_msg(std::source_location const &loc, std::string const &msg)
 {
     return std::format("{}, {}, {}\n{}", loc.file_name(), loc.function_name(), loc.line(), msg);
+}
+
+inline std::string create_error_msg(std::source_location const &loc)
+{
+    return std::format("{}, {}, {}:", loc.file_name(), loc.function_name(), loc.line());
 }
 
 }
