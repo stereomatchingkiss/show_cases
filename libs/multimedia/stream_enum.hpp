99cd22e11a74f326bcd785f7061ab412ac7979f9
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-07 16:48:41 +0800

    add new type

--- libs/multimedia/stream_enum.hpp
+++ libs/multimedia/stream_enum.hpp
@@ -4,14 +4,16 @@ namespace flt::mm{
 
 enum class stream_receiver_type : int
 {
-   opencv
+   opencv,
+   websocket
 };
 
 enum class stream_source_type : int
 {
     webcam,
     video,
-    rtsp
+    rtsp,
+    websocket
 };
 
 }
