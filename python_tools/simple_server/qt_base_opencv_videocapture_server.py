5338f6c063fa9a88b40d9a47bb376f93c75a27f0
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-07 19:23:20 +0800

    can select fps

--- python_tools/simple_server/qt_base_opencv_videocapture_server.py
+++ python_tools/simple_server/qt_base_opencv_videocapture_server.py
@@ -1,14 +1,15 @@
 from PyQt5.QtNetwork import QHostAddress
 from PyQt5.QtWebSockets import QWebSocketServer
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QTimer
 
 import argparse
-import base64
 import cv2
 import sys
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--downsample", required=False, type=int, default=1)
+ap.add_argument("--max_fps", required=False, type=int, default=30, help="Increase the number if you want the server process the frame faster, maximum value is 1000")
 ap.add_argument("--port", required=False, type=int, default=8000)
 ap.add_argument("--opencv_url", type=str, required=True,
                 help="Url of the video, could be local location of video, rtsp address, or enter number(ex : 0) to open webcam")
@@ -20,14 +21,19 @@ class simple_websocket_server(QWidget):
     def __init__(self, port : int = args["port"], parent = None):
         super(simple_websocket_server, self).__init__(parent)
 
+        self.camera = None
         self.downsample = args["downsample"]
+        self.max_fps = args["max_fps"]
         self.port = port
         self.server = QWebSocketServer("Simple websocket server", QWebSocketServer.NonSecureMode, self)
         self.socket = None
+        self.timer = QTimer()
+        self.timer.setInterval(1000.0/self.max_fps)
 
         if(self.server.listen(QHostAddress.Any, port)):
             print("SSL Echo Server listening on port: ", port)
             self.server.newConnection.connect(self.on_new_connection)
+            self.timer.timeout.connect(self.send_frame)
 
     def on_new_connection(self):
 
@@ -42,32 +48,30 @@ class simple_websocket_server(QWidget):
         self.send_frame()
 
     def send_frame(self):
-        camera = cv2.VideoCapture(args["opencv_url"], )
-        if(camera.isOpened() == False):
-            print("cannot open camera from the url = ", args["opencv_url"])
-            return
+        if self.camera is None:
+            self.camera = cv2.VideoCapture(args["opencv_url"], )
+            if(self.camera.isOpened() == False):
+                print("cannot open camera from the url = ", args["opencv_url"])
+                self.timer.stop()
+                return
+            else:
+                self.timer.start()
 
-        while True:
-            try:
-                _, frame = camera.read()
-                if frame is None:
-                    print("cannot capture frame")
-                    break
-                if self.socket is None:
-                    break
+        try:
+            _, frame = self.camera.read()
+            if frame is None:
+                print("cannot capture frame")
+            if self.socket:
+                if args["downsample"] != 1:
+                    height, width, _ = frame.shape
+                    frame = cv2.resize(frame, (int(width / self.downsample), int(height / self.downsample)))
 
-                if self.socket:
-                    if args["downsample"] != 1:
-                        height, width, _ = frame.shape
-                        frame = cv2.resize(frame, (int(width / self.downsample), int(height / self.downsample)))
+                _, buffer = cv2.imencode('.jpg', frame)
+                self.socket.sendBinaryMessage(buffer.tobytes())
 
-                    _, buffer = cv2.imencode('.jpg', frame)
-                    self.socket.sendBinaryMessage(buffer.tobytes())
-
-            except:
-                camera.release()
-                cv2.destroyAllWindows()
-                break
+        except:
+            self.camera.release()
+            cv2.destroyAllWindows()
 
     def socket_disconnected(self):
         print("socket disconnected")
@@ -75,6 +79,10 @@ class simple_websocket_server(QWidget):
         if self.socket:
             self.socket.deleteLater()
             self.socket = None
+        if self.camera:
+            self.camera.release()
+            cv2.destroyAllWindows()
+            self.camera = None
 
 def main():
     app = QApplication(sys.argv)
--- python_tools/simple_server/readme.md
+++ python_tools/simple_server/readme.md
@@ -14,4 +14,5 @@ python qt_base_opencv_videocapture_server.py --opencv_url your_url/your_video.mp
 
 . This simple server accept one client connection only
 . If the frame is too large, please enter "--downsample 2" to scale down the image, 2 mean scale down 2 times, 3 mean times etc
+. For simplicity, this simple server do not perform any error handling of socket connection
 
