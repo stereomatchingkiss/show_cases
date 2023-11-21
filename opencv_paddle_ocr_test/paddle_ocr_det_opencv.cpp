21baac6fcc862fdc594f16f96e19800475d1306b
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-21 08:29:12 +0800

    remove useless codes

--- opencv_paddle_ocr_test/paddle_ocr_det_opencv.cpp
+++ opencv_paddle_ocr_test/paddle_ocr_det_opencv.cpp
@@ -18,18 +18,12 @@ namespace{
 // Get Output Layers Name
 std::vector<std::string> getOutputsNames( const cv::dnn::Net& net )
 {
-    static std::vector<std::string> names;
-    if( names.empty() ){
-        std::vector<int32_t> out_layers = net.getUnconnectedOutLayers();
-        std::vector<std::string> layers_names = net.getLayerNames();
-        names.resize( out_layers.size() );
-        for( size_t i = 0; i < out_layers.size(); ++i ){
-            names[i] = layers_names[out_layers[i] - 1];
-        }
-    }
-
-    for(auto const &val : names){
-        std::cout<<"output name = "<<val<<std::endl;
+    std::vector<std::string> names;
+    std::vector<int32_t> const out_layers = net.getUnconnectedOutLayers();
+    std::vector<std::string> const layers_names = net.getLayerNames();
+    names.resize( out_layers.size() );
+    for( size_t i = 0; i < out_layers.size(); ++i ){
+        names[i] = layers_names[out_layers[i] - 1];
     }
 
     return names;
@@ -37,24 +31,23 @@ std::vector<std::string> getOutputsNames( const cv::dnn::Net& net )
 
 
 // resize image to a size multiple of 32 which is required by the network
-cv::Mat DetResizeImg(const cv::Mat img,
-                     int max_size_len,
-                     std::array<float, 3> &ratio_hw) {
-    int w = img.cols;
-    int h = img.rows;
-
-    ratio_hw[2] = 1.f;
-    int max_wh = w >= h ? w : h;
+std::tuple<std::array<float, 3>, cv::Size> get_resize_params(cv::Mat const &img, int max_size_len)
+{
+    int const w = img.cols;
+    int const h = img.rows;
+    std::array<float, 3> ratio_hws;
+    ratio_hws[2] = 1.f;
+    int const max_wh = w >= h ? w : h;
     if (max_wh > max_size_len) {
         if (h > w) {
-            ratio_hw[2] = static_cast<float>(max_size_len) / static_cast<float>(h);
+            ratio_hws[2] = static_cast<float>(max_size_len) / static_cast<float>(h);
         } else {
-            ratio_hw[2] = static_cast<float>(max_size_len) / static_cast<float>(w);
+            ratio_hws[2] = static_cast<float>(max_size_len) / static_cast<float>(w);
         }
     }
 
-    int resize_h = static_cast<int>(float(h) * ratio_hw[2]);
-    int resize_w = static_cast<int>(float(w) * ratio_hw[2]);
+    int resize_h = static_cast<int>(float(h) * ratio_hws[2]);
+    int resize_w = static_cast<int>(float(w) * ratio_hws[2]);
     if (resize_h / 32 < 1 + 1e-5)
         resize_h = 32;
     else
@@ -65,19 +58,16 @@ cv::Mat DetResizeImg(const cv::Mat img,
     else
         resize_w = (resize_w / 32 - 1) * 32;
 
-    cv::Mat resize_img;
-    cv::resize(img, resize_img, cv::Size(resize_w, resize_h));
+    ratio_hws[0] = (static_cast<float>(resize_h) / static_cast<float>(h));
+    ratio_hws[1] = (static_cast<float>(resize_w) / static_cast<float>(w));
 
-    ratio_hw[0] = (static_cast<float>(resize_h) / static_cast<float>(h));
-    ratio_hw[1] = (static_cast<float>(resize_w) / static_cast<float>(w));
-
-    return resize_img;
+    return {ratio_hws, cv::Size(resize_w, resize_h)};
 }
 
 }
 
 paddle_ocr_det_opencv::paddle_ocr_det_opencv(const std::string &model_weights,
-                                             float binary_map_thresh,
+                                             double binary_map_thresh,
                                              float box_thresh,
                                              float unclip_ratio,
                                              bool use_dilate) :
@@ -85,7 +75,8 @@ paddle_ocr_det_opencv::paddle_ocr_det_opencv(const std::string &model_weights,
     box_thresh_{box_thresh},
     unclip_ratio_{unclip_ratio},
     use_dilate_{use_dilate},
-    net_{cv::dnn::readNet(model_weights)}
+    net_{cv::dnn::readNet(model_weights)},
+    output_names_{getOutputsNames(net_)}
 {
     net_ = cv::dnn::readNet(model_weights);
 }
@@ -97,7 +88,7 @@ paddle_ocr_det_opencv::~paddle_ocr_det_opencv()
 
 std::vector<flt::cvt::ocr::TextBox> paddle_ocr_det_opencv::predict(const cv::Mat &input)
 {
-    std::map<std::string, double> config{
+    /*std::map<std::string, double> config{
         {"det_db_box_thresh", 0.5},
         {"det_db_thresh", 0.3},
         {"det_db_unclip_ratio", 1.6},
@@ -106,24 +97,22 @@ std::vector<flt::cvt::ocr::TextBox> paddle_ocr_det_opencv::predict(const cv::Mat
         {"max_side_len", 960},
         {"rec_image_height", 48},
         {"use_direction_classify", 1}
-    };
+    };//*/
 
-    return RunDetModel(net_, input, config);
+    return RunDetModel(net_, input);
 }
 
-std::vector<flt::cvt::ocr::TextBox> paddle_ocr_det_opencv::RunDetModel(cv::dnn::Net &net,
-                                                                              cv::Mat const &img,
-                                                                              std::map<std::string, double> &Config)
+std::vector<flt::cvt::ocr::TextBox> paddle_ocr_det_opencv::RunDetModel(cv::dnn::Net &net, cv::Mat const &img)
 {
     // Read img
     int constexpr max_side_len = 960;
     int const origin_row = img.rows;
     int const origin_col = img.cols;
 
+    cv::Size resize_size;
     std::array<float, 3> ratio_hw;
-    auto resize_img = DetResizeImg(img, max_side_len, ratio_hw);
-
-    cv::Mat img_fp = cv::dnn::blobFromImage(resize_img, 1.0f / 255.0f, resize_img.size(), cv::Scalar(123, 117, 104), false, false);
+    std::tie(ratio_hw, resize_size) = get_resize_params(img, max_side_len);
+    cv::Mat img_fp = cv::dnn::blobFromImage(img, 1.0f / 255.0f, resize_size, cv::Scalar(123, 117, 104), false, false);
     {
         auto ptr = img_fp.ptr<float>(0);
         float constexpr scale[] = {1 / 0.229f, 1 / 0.224f, 1 / 0.225f};
@@ -149,14 +138,14 @@ std::vector<flt::cvt::ocr::TextBox> paddle_ocr_det_opencv::RunDetModel(cv::dnn::
     auto outptr = det_out.ptr<float>(0);
     auto cbuf_map_ptr = cbuf_map.ptr<uchar>(0);
     auto pred_map_ptr = pred_map.ptr<float>(0);
-    for(int i = 0; i < int(shape_out[2] * shape_out[3]); ++i) {
+    for(int i = 0; i < shape_out[2] * shape_out[3]; ++i) {
         pred_map_ptr[i] = static_cast<float>(outptr[i]);
         cbuf_map_ptr[i] = static_cast<unsigned char>((outptr[i]) * 255);
     }
 
     //binary_map_thresh
-    const double threshold = double(Config["det_db_thresh"]) * 255;
-    const double max_value = 255;
+    double const threshold = binary_map_thresh_ * 255;
+    double const max_value = 255.0;
     cv::Mat bit_map;
     cv::threshold(cbuf_map, bit_map, threshold, max_value, cv::THRESH_BINARY);
 
