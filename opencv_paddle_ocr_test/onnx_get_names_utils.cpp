db49d389872b75964913b5587eaf869b4725bd29
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-22 12:04:00 +0800

    first commit

---
+++ opencv_paddle_ocr_test/onnx_get_names_utils.cpp
@@ -0,0 +1,90 @@
+#include "onnx_get_names_utils.hpp"
+
+#include <iostream>
+
+onnx_get_names_utils::onnx_get_names_utils()
+{
+
+}
+
+std::vector<int64_t>& onnx_get_names_utils::input_node_dims() noexcept
+{
+    return input_node_dims_;
+}
+
+const std::vector<Ort::AllocatedStringPtr> &onnx_get_names_utils::input_names_ptr() const noexcept
+{
+    return input_names_ptr_;
+}
+
+const std::vector<const char *> &onnx_get_names_utils::input_node_names() const noexcept
+{
+    return input_node_names_;
+}
+
+std::vector<int64_t>& onnx_get_names_utils::output_node_dims() noexcept
+{
+    return output_node_dims_;
+}
+
+const std::vector<Ort::AllocatedStringPtr> &onnx_get_names_utils::output_names_ptr() const noexcept
+{
+    return output_names_ptr_;
+}
+
+const std::vector<const char *> &onnx_get_names_utils::output_node_names() const noexcept
+{
+    return output_node_names_;
+}
+
+void onnx_get_names_utils::process(const Ort::Session &session)
+{
+    get_input_names(session);
+    get_output_names(session);
+}
+
+void onnx_get_names_utils::print_model_details()
+{
+    for(size_t j = 0; j < input_node_dims_.size(); ++j){
+        std::cout << "Input " << j << " : dim[" << j << "] =" << input_node_dims_[j] << '\n';
+    }
+    for(size_t j = 0; j != input_node_names_.size(); ++j){
+        std::cout<<"input names "<< j <<":"<<input_node_names_[j]<<"\n";
+    }
+
+    for(size_t j = 0; j < output_node_dims_.size(); ++j){
+        std::cout << "Output " << j << " : dim[" << j << "] =" << output_node_dims_[j] << '\n';
+    }
+    for(size_t j = 0; j != output_node_names_.size(); ++j){
+        std::cout<<"output names "<< j <<":"<<output_node_names_[j]<<"\n";
+    }
+    std::cout << std::flush;
+}
+
+void onnx_get_names_utils::get_names(size_t node_index, std::vector<int64_t> &node_dims, std::vector<Ort::AllocatedStringPtr> &names_ptr, std::vector<const char *> &node_names, const Ort::Session &session, bool input) const
+{
+    size_t const node_counts = input ? session.GetInputCount() : session.GetOutputCount();
+    names_ptr.reserve(node_counts);
+    node_names.reserve(node_counts);
+
+    Ort::AllocatorWithDefaultOptions allocator;
+    auto node_name = input ? session.GetInputNameAllocated(node_index, allocator) :
+                         session.GetOutputNameAllocated(node_index, allocator);
+    node_names.push_back(node_name.get());
+    names_ptr.push_back(std::move(node_name));
+
+    auto const type_info = session.GetInputTypeInfo(node_index);
+    auto const tensor_info = type_info.GetTensorTypeAndShapeInfo();
+
+    node_dims = tensor_info.GetShape();
+}
+
+void onnx_get_names_utils::get_input_names(const Ort::Session &session)
+{
+    get_names(0, input_node_dims_, input_names_ptr_, input_node_names_, session, true);
+}
+
+void onnx_get_names_utils::get_output_names(const Ort::Session &session)
+{
+    get_names(0, output_node_dims_, output_names_ptr_, output_node_names_, session, false);
+}
---
+++ opencv_paddle_ocr_test/onnx_get_names_utils.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <onnxruntime_cxx_api.h>
+
+#include <string>
+#include <vector>
+
+class onnx_get_names_utils
+{
+public:
+    onnx_get_names_utils();
+
+    std::vector<int64_t>& input_node_dims() noexcept;
+    std::vector<Ort::AllocatedStringPtr> const& input_names_ptr() const noexcept;
+    std::vector<const char*> const& input_node_names() const noexcept;
+
+    std::vector<int64_t>& output_node_dims() noexcept;
+    std::vector<Ort::AllocatedStringPtr> const& output_names_ptr() const noexcept;
+    std::vector<const char*> const& output_node_names() const noexcept;
+
+    void process(Ort::Session const &session);
+
+    void print_model_details();
+
+private:        
+    void get_names(size_t node_index,
+                   std::vector<int64_t> &node_dims,
+                   std::vector<Ort::AllocatedStringPtr> &names_ptr,
+                   std::vector<const char*> &node_names,
+                   Ort::Session const &session,
+                   bool input) const;
+
+    void get_input_names(Ort::Session const &session);
+
+    void get_output_names(Ort::Session const &session);
+
+    std::vector<int64_t> input_node_dims_;
+    std::vector<Ort::AllocatedStringPtr> input_names_ptr_;
+    std::vector<const char*> input_node_names_;
+
+    std::vector<int64_t> output_node_dims_;
+    std::vector<Ort::AllocatedStringPtr> output_names_ptr_;
+    std::vector<const char*> output_node_names_;
+};
