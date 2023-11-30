cf45d564a5707a8a571dfd1d8f3e2f14fc791d56
stereomatchingkiss <thamngapwei@gmail.com>
2023-11-30 17:39:45 +0800

    add new widget

--- simple_downloader/mainwindow.cpp
+++ simple_downloader/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "./ui_mainwindow.h"
 
 #include "widget_download_imgs_from_flist.hpp"
+#include "widget_gdrive_downloader.hpp"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,6 +12,9 @@ MainWindow::MainWindow(QWidget *parent)
 
     widget_ = new widget_download_imgs_from_flist;
     ui->stackedWidget->addWidget(widget_);
+
+    ui->comboBoxFunctions->addItem(tr("Download imgs from file list"));
+    ui->comboBoxFunctions->addItem(tr("Download file from gdrive"));
 }
 
 MainWindow::~MainWindow()
@@ -20,14 +24,26 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_comboBoxFunctions_activated(int index)
 {
-    if(ui->comboBoxFunctions->currentIndex() != index){
+    qDebug()<<"current box index = "<<index;
+    if(current_index_ != index){
+        qDebug()<<"switch widget = "<<index;
         ui->stackedWidget->removeWidget(widget_);
         widget_->deleteLater();
+        widget_ = nullptr;
 
         if(index == 0){
             widget_ = new widget_download_imgs_from_flist;
             ui->stackedWidget->addWidget(widget_);
+        }else if(index == 1){
+            widget_ = new widget_gdrive_downloader;
+            ui->stackedWidget->addWidget(widget_);
         }
+
+        if(widget_){
+            ui->stackedWidget->setCurrentWidget(widget_);
+        }
+
+        current_index_ = index;
     }
 }
 
