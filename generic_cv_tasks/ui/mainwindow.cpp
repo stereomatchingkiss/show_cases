70e458306682ced87a473b9fa65812ea5479ac5f
stereomatchingkiss <thamngapwei@gmail.com>
2023-12-15 07:30:14 +0800

    move pages into another classes, easier to maintain and support multi-streams

--- generic_cv_tasks/ui/mainwindow.cpp
+++ generic_cv_tasks/ui/mainwindow.cpp
@@ -2,38 +2,19 @@
 #include "./ui_mainwindow.h"
 
 #include "widget_alert_sender_settings.hpp"
-#include "widget_object_detect_model_select.hpp"
-#include "widget_select_object_to_detect.hpp"
-#include "widget_source_selection.hpp"
-#include "widget_stream_player.hpp"
-#include "widget_tracker_alert.hpp"
 
-#include "../algo/frame_display_worker.hpp"
-#include "../algo/obj_detect/nanodet_worker.hpp"
+#include "widget_stacks_manager.hpp"
 
-#include "../config/config_nanodet_worker.hpp"
+#include "../config/config_alert_sender.hpp"
 #include "../config/config_read_write.hpp"
 
 #include "../global/global_keywords.hpp"
 #include "../global/global_object.hpp"
 
-#include <multimedia/camera/frame_capture_qcamera.hpp>
-#include <multimedia/camera/frame_capture_qcamera_params.hpp>
-#include <multimedia/camera/frame_capture_qmediaplayer.hpp>
-#include <multimedia/camera/frame_capture_qmediaplayer_params.hpp>
-#include <multimedia/camera/frame_process_controller.hpp>
-#include <multimedia/network/frame_capture_websocket.hpp>
-#include <multimedia/network/frame_capture_websocket_params.hpp>
-#include <multimedia/sound/alert_sound_manager.hpp>
-#include <multimedia/stream_enum.hpp>
-
 #include <network/websocket_client_controller.hpp>
-
-#include <ui/label_select_roi.hpp>
 
 #include <QFileDialog>
 #include <QMessageBox>
-#include <QTimer>
 
 #include <QJsonDocument>
 #include <QJsonObject>
@@ -45,15 +26,10 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)    
     , msg_box_(new QMessageBox(this))
-    , timer_(new QTimer(this))
-    , websocket_(std::make_unique<flt::net::websocket_client_controller>())
 {
     ui->setupUi(this);
 
     init_stacked_widget();
-
-    ui->pushButtonPrev->setEnabled(false);
-    ui->labelTitle->setText(tr("Select model"));
 
     connect(ui->actionContactMe, &QAction::triggered, this, &MainWindow::action_contact_me);
     connect(ui->actionQt, &QAction::triggered, this, &MainWindow::action_about_qt);
@@ -61,18 +37,16 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->actionSaveSettings, &QAction::triggered, this, &MainWindow::save_settings);
     connect(ui->actionReadMe, &QAction::triggered, this, &MainWindow::action_warning);
     connect(ui->actionServer, &QAction::triggered, this, &MainWindow::action_server_call);
-    connect(&get_widget_alert_sender_settings(), &widget_alert_sender_settings::button_ok_clicked, [this](auto const &val)
+    connect(&get_widget_alert_sender_settings(), &widget_alert_sender_settings::button_ok_clicked, [](auto const &val)
             {
                 if(get_widget_alert_sender_settings().get_config().activate_){
-                    emit websocket_->reopen_if_needed(val.url_);
+                    emit get_websocket_controller().reopen_if_needed(val.url_);
                 }
             });
 
-    emit websocket_->initialize();
+    emit get_websocket_controller().initialize();
 
-    setMinimumSize(QSize(600, 400));
-
-    timer_->setInterval(1000);
+    setMinimumSize(QSize(600, 400));    
 }
 
 MainWindow::~MainWindow()
@@ -80,57 +54,6 @@ MainWindow::~MainWindow()
     save_settings_to_file(global_keywords().cam_config_path() + "/cam0.json");
 
     delete ui;
-}
-
-void MainWindow::on_pushButtonNext_clicked()
-{
-    if(ui->stackedWidget->currentWidget() == widget_object_detect_model_select_){
-        get_alert_sound_manager();
-        next_page_is_widget_select_object_to_detect();
-    }else if(ui->stackedWidget->currentWidget() == widget_select_object_to_detect_){
-        next_page_is_widget_tracker_alert();
-    }else if(ui->stackedWidget->currentWidget() == widget_tracker_alert_){
-        next_page_is_widget_source_selection();
-    }else if(ui->stackedWidget->currentWidget() == widget_source_selection_){
-        next_page_is_label_select_roi();
-    }else if(ui->stackedWidget->currentWidget() == label_select_roi_){
-        next_page_is_widget_stream_player();
-    }
-}
-
-
-void MainWindow::on_pushButtonPrev_clicked()
-{
-    sfwmw_.reset();
-    ui->labelTitle->setVisible(true);
-    if(ui->stackedWidget->currentWidget() == widget_select_object_to_detect_){
-        ui->labelTitle->setText(tr("Select model"));
-        ui->stackedWidget->setCurrentWidget(widget_object_detect_model_select_);
-        ui->pushButtonNext->setEnabled(true);
-        ui->pushButtonPrev->setEnabled(false);
-    }else if(ui->stackedWidget->currentWidget() == widget_source_selection_){
-        ui->labelTitle->setText(tr("Alert settings"));
-        ui->stackedWidget->setCurrentWidget(widget_tracker_alert_);
-        ui->pushButtonNext->setEnabled(true);
-        ui->pushButtonPrev->setEnabled(true);
-    }else if(ui->stackedWidget->currentWidget() == widget_tracker_alert_){
-        ui->labelTitle->setText(tr("Select objects to detect"));
-        ui->stackedWidget->setCurrentWidget(widget_select_object_to_detect_);
-        ui->pushButtonNext->setEnabled(true);
-        ui->pushButtonPrev->setEnabled(true);
-    }else if(ui->stackedWidget->currentWidget() == label_select_roi_){
-        ui->labelTitle->setText(tr("Select stream"));
-        ui->stackedWidget->setCurrentWidget(widget_source_selection_);
-        ui->pushButtonNext->setEnabled(true);
-        ui->pushButtonPrev->setEnabled(true);
-    }else if(ui->stackedWidget->currentWidget() == widget_stream_player_){
-        ui->labelTitle->setText(tr("Select roi"));
-        ui->stackedWidget->setCurrentWidget(label_select_roi_);
-        ui->pushButtonNext->setEnabled(true);
-        ui->pushButtonPrev->setEnabled(true);
-
-        create_roi_select_stream();
-    }
 }
 
 void MainWindow::action_about_qt(bool)
@@ -162,137 +85,18 @@ void MainWindow::action_warning(bool)
                          "use of this software."));
 }
 
-void MainWindow::create_frame_capture()
-{
-    timer_->stop();
-    disconnect(timer_, &QTimer::timeout, this, &MainWindow::update_position);
-    if(widget_source_selection_->get_source_type() == stream_source_type::websocket){
-        sfwmw_ = std::make_unique<frame_capture_websocket>(widget_source_selection_->get_frame_capture_websocket_params());
-    }else if(widget_source_selection_->get_source_type() == stream_source_type::webcam){                
-        sfwmw_ = std::make_unique<frame_capture_qcamera>(widget_source_selection_->get_frame_capture_qcamera_params());
-    }else{
-        sfwmw_ = std::make_unique<frame_capture_qmediaplayer>(widget_source_selection_->get_frame_capture_qmediaplayer_params());
-        auto player = static_cast<frame_capture_qmediaplayer*>(sfwmw_.get());
 
-        connect(widget_stream_player_, &widget_stream_player::pause, player, &frame_capture_qmediaplayer::pause);
-        connect(widget_stream_player_, &widget_stream_player::play, player, &frame_capture_qmediaplayer::start);
-        connect(widget_stream_player_, &widget_stream_player::seek, player, &frame_capture_qmediaplayer::set_position);
-
-        connect(timer_, &QTimer::timeout, this, &MainWindow::update_position);
-    }
-}
-
-void MainWindow::create_roi_select_stream()
-{
-    create_frame_capture();
-    auto process_controller = std::make_shared<frame_process_controller>(new frame_display_worker);
-    connect(process_controller.get(), &frame_process_controller::send_process_results,
-            label_select_roi_, &ui::label_select_roi::display_frame);
-    emit process_controller->start();
-    sfwmw_->add_listener(process_controller, this);
-    sfwmw_->start();
-}
 
 void MainWindow::init_stacked_widget()
-{
-    label_select_roi_ = new ui::label_select_roi(tr("Select roi"));
-    widget_object_detect_model_select_ = new widget_object_detect_model_select;
-    widget_source_selection_ = new widget_source_selection;
-    widget_stream_player_    = new widget_stream_player;
-    widget_tracker_alert_    = new widget_tracker_alert;
-
-    global_keywords gk;
-    widget_select_object_to_detect_ = new widget_select_object_to_detect(gk.coco_names());
-
-    init_widgets_states(global_keywords().cam_config_path() + "/cam0.json");
-
-    ui->stackedWidget->addWidget(label_select_roi_);
-    ui->stackedWidget->addWidget(widget_object_detect_model_select_);
-    ui->stackedWidget->addWidget(widget_select_object_to_detect_);
-    ui->stackedWidget->addWidget(widget_source_selection_);
-    ui->stackedWidget->addWidget(widget_stream_player_);
-    ui->stackedWidget->addWidget(widget_tracker_alert_);
-
-    ui->stackedWidget->setCurrentWidget(widget_object_detect_model_select_);
-}
-
-void MainWindow::next_page_is_widget_stream_player()
-{
-    ui->labelTitle->setVisible(false);
-    ui->stackedWidget->setCurrentWidget(widget_stream_player_);
-    ui->pushButtonNext->setEnabled(false);
-    ui->pushButtonPrev->setEnabled(true);
-
-    config_nanodet_worker config;
-    config.config_alert_sender_ = get_widget_alert_sender_settings().get_config();
-    config.config_object_detect_model_select_ = widget_object_detect_model_select_->get_config();
-    config.config_select_object_to_detect_ = widget_select_object_to_detect_->get_config();
-    config.roi_ = label_select_roi_->get_norm_rubber_band_rect();
-    config.config_tracker_alert_ = widget_tracker_alert_->get_config();
-
-    auto worker = new nanodet_worker(std::move(config));
-    connect(&get_widget_alert_sender_settings(), &widget_alert_sender_settings::button_ok_clicked,
-            worker, &nanodet_worker::change_alert_sender_config);
-    connect(worker, &nanodet_worker::send_alert_by_binary, this, &MainWindow::send_alert_by_binary);
-    connect(worker, &nanodet_worker::send_alert_by_text, this, &MainWindow::send_alert_by_text);
-
-    auto process_controller = std::make_shared<frame_process_controller>(worker);
-    connect(process_controller.get(), &frame_process_controller::send_process_results,
-            widget_stream_player_, &widget_stream_player::display_frame);
-
-    if(get_widget_alert_sender_settings().get_config().activate_){
-        emit websocket_->reopen_if_needed(get_widget_alert_sender_settings().get_config().url_);
-    }
-    create_frame_capture();
-    emit process_controller->start();
-    sfwmw_->add_listener(process_controller, this);
-    sfwmw_->start();
-
-    if(widget_source_selection_->get_source_type() == stream_source_type::hls ||
-        widget_source_selection_->get_source_type() == stream_source_type::video){
-        auto player = static_cast<frame_capture_qmediaplayer*>(sfwmw_.get());
-        widget_stream_player_->set_is_seekable(player->is_seekable());
-        widget_stream_player_->set_duration(player->position(), player->max_position());
-        connect(timer_, &QTimer::timeout, this, &MainWindow::update_position);
-        timer_->start();
-    }else{
-        widget_stream_player_->set_is_seekable(false);
-    }
-}
-
-void MainWindow::next_page_is_widget_select_object_to_detect()
-{
-    ui->labelTitle->setText(tr("Select objects to detect"));
-    ui->stackedWidget->setCurrentWidget(widget_select_object_to_detect_);
-    ui->pushButtonNext->setEnabled(true);
-    ui->pushButtonPrev->setEnabled(true);
-}
-
-void MainWindow::next_page_is_widget_source_selection()
-{
-    ui->labelTitle->setText(tr("Select steram"));
-    ui->stackedWidget->setCurrentWidget(widget_source_selection_);
-    ui->pushButtonNext->setEnabled(true);
-    ui->pushButtonPrev->setEnabled(true);
-}
-
-void MainWindow::next_page_is_widget_tracker_alert()
-{
-    ui->labelTitle->setText(tr("Alert settings"));
-    ui->stackedWidget->setCurrentWidget(widget_tracker_alert_);
-    ui->pushButtonNext->setEnabled(true);
-    ui->pushButtonPrev->setEnabled(true);
+{    
+    widget_stacks_manager_ = new widget_stacks_manager;
+    ui->stackedWidget->addWidget(widget_stacks_manager_);
 }
 
 QJsonObject MainWindow::dump_settings() const
 {
-    config_read_write crw;
-    crw.set_roi(label_select_roi_->get_states());
+    config_read_write crw;    
     crw.set_widget_alert_settings(get_widget_alert_sender_settings().get_states());
-    crw.set_widget_object_detect_model_select(widget_object_detect_model_select_->get_states());
-    crw.set_widget_select_object_to_detect(widget_select_object_to_detect_->get_states());
-    crw.set_widget_source_selection(widget_source_selection_->get_states());
-    crw.set_widget_tracker_alert(widget_tracker_alert_->get_states());
 
     return crw.dumps();
 }
@@ -320,13 +124,8 @@ void MainWindow::init_widgets_states(const QString &fname)
 
 void MainWindow::init_widgets_states(const QJsonObject &jobj)
 {
-    global_keywords gk;
-    label_select_roi_->set_states(jobj[gk.state_roi()].toObject());
+    global_keywords gk;    
     get_widget_alert_sender_settings().set_states(jobj[gk.state_widget_alert_settings()].toObject());
-    widget_object_detect_model_select_->set_states(jobj[gk.state_widget_object_detect_model_select()].toObject());
-    widget_select_object_to_detect_->set_states(jobj[gk.state_widget_select_object_to_detect()].toObject());
-    widget_source_selection_->set_states(jobj[gk.state_widget_source_selection()].toObject());
-    widget_tracker_alert_->set_states(jobj[gk.state_tracker_alert()].toObject());
 }
 
 void MainWindow::save_settings(bool)
@@ -349,37 +148,4 @@ void MainWindow::save_settings(bool)
 void MainWindow::save_settings_to_file(const QString &save_at) const
 {
     config_read_write().write(dump_settings(), save_at);
-}
-
-void MainWindow::send_alert_by_binary(const QByteArray &msg)
-{
-    emit websocket_->send_binary_message(msg);
-}
-
-void MainWindow::send_alert_by_text(const QString &msg)
-{
-    emit websocket_->send_text_message(msg);
-}
-
-void MainWindow::update_position()
-{
-    if(sfwmw_){
-        auto player = static_cast<frame_capture_qmediaplayer*>(sfwmw_.get());
-        widget_stream_player_->set_current_position(player->position());
-    }
-}
-
-void MainWindow::next_page_is_label_select_roi()
-{
-    if(!widget_source_selection_->get_is_valid_source()){
-        msg_box_->warning(this, tr("Warning"), tr("Invalid url"));
-        msg_box_->show();
-    }else{
-        ui->labelTitle->setText(tr("Select roi"));
-        ui->stackedWidget->setCurrentWidget(label_select_roi_);
-        ui->pushButtonNext->setEnabled(true);
-        ui->pushButtonPrev->setEnabled(true);
-
-        create_roi_select_stream();
-    }
 }
--- generic_cv_tasks/ui/mainwindow.hpp
+++ generic_cv_tasks/ui/mainwindow.hpp
@@ -1,40 +1,14 @@
-#ifndef MAINWINDOW_HPP
-#define MAINWINDOW_HPP
+#pragma once
 
 #include <QMainWindow>
-
-#include <memory>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class widget_object_detect_model_select;
-class widget_select_object_to_detect;
-class widget_source_selection;
-class widget_stream_player;
-class widget_tracker_alert;
+class widget_stacks_manager;
 
 class QMessageBox;
-class QTimer;
-
-namespace flt::mm{
-
-class single_frame_with_multi_worker_base;
-
-}
-
-namespace flt::net{
-
-class websocket_client_controller;
-
-}
-
-namespace flt::ui{
-
-class label_select_roi;
-
-}
 
 class MainWindow : public QMainWindow
 {
@@ -44,52 +18,24 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_pushButtonNext_clicked();
-
-    void on_pushButtonPrev_clicked();
-
 private:
     void action_about_qt(bool);
     void action_contact_me(bool);
     void action_server_call();
     void action_warning(bool);
 
-    void create_frame_capture();
-    void create_roi_select_stream();
-
     void init_stacked_widget();
-
-    void next_page_is_label_select_roi();
-    void next_page_is_widget_stream_player();
-    void next_page_is_widget_select_object_to_detect();
-    void next_page_is_widget_source_selection();
-    void next_page_is_widget_tracker_alert();
 
     QJsonObject dump_settings() const;
     void load_settings(bool);
     void init_widgets_states(QString const &fname);
     void init_widgets_states(QJsonObject const &jobj);
     void save_settings(bool);
-    void save_settings_to_file(QString const &save_at) const;
-
-    void send_alert_by_binary(QByteArray const &msg);
-    void send_alert_by_text(QString const &msg);
-
-    void update_position();
+    void save_settings_to_file(QString const &save_at) const;    
 
     Ui::MainWindow *ui;
-        
-    flt::ui::label_select_roi *label_select_roi_;
-    QMessageBox *msg_box_;
-    QTimer *timer_;
-    widget_object_detect_model_select *widget_object_detect_model_select_;
-    widget_select_object_to_detect *widget_select_object_to_detect_;
-    widget_source_selection *widget_source_selection_;
-    widget_stream_player *widget_stream_player_;
-    widget_tracker_alert *widget_tracker_alert_;   
+            
+    QMessageBox *msg_box_;    
 
-    std::unique_ptr<flt::mm::single_frame_with_multi_worker_base> sfwmw_;    
-    std::unique_ptr<flt::net::websocket_client_controller> websocket_;
+    widget_stacks_manager *widget_stacks_manager_;
 };
-#endif // MAINWINDOW_HPP
