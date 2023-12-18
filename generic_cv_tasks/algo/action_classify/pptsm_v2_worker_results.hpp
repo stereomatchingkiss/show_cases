f6f3931bf76fcbee291578fdc7af5c2915a83eff
stereomatchingkiss <thamngapwei@gmail.com>
2023-12-18 15:05:54 +0800

    remove useless files

--- generic_cv_tasks/algo/action_classify/pptsm_v2_worker_results.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QImage>
-
-struct pptsm_v2_worker_results
-{
-    bool alarm_on_ = false;
-    QImage mat_;
-};
--- generic_cv_tasks/algo/obj_detect/obj_det_worker_results.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QImage>
-
-struct obj_det_worker_results
-{
-    bool alarm_on_ = false;
-    QImage mat_;
-};
