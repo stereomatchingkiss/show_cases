bdc4cc36505c21614e4510a5f4eb81ffa3b78888
stereomatchingkiss <thamngapwei@gmail.com>
2023-12-18 15:06:34 +0800

    replace structure

--- generic_cv_tasks/ui/widget_stream_player.cpp
+++ generic_cv_tasks/ui/widget_stream_player.cpp
@@ -1,7 +1,7 @@
 #include "widget_stream_player.hpp"
 #include "ui_widget_stream_player.h"
 
-#include "../algo/obj_detect/obj_det_worker_results.hpp"
+#include "../algo/generic_worker_results.hpp"
 #include "../global/global_object.hpp"
 
 #include <multimedia/sound/alert_sound_manager.hpp>
@@ -55,7 +55,7 @@ widget_stream_player::~widget_stream_player()
 
 void widget_stream_player::display_frame(std::any results)
 {
-    auto const val = std::any_cast<obj_det_worker_results>(results);
+    auto const val = std::any_cast<generic_worker_results>(results);
     int const w = ui->labelStream->width();
     int const h = ui->labelStream->height();
 
