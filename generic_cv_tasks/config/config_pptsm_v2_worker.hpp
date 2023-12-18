15cfbfea6269fe3dae8918e88b476deae2b9bf94
stereomatchingkiss <thamngapwei@gmail.com>
2023-12-18 14:11:53 +0800

    add alert config

--- generic_cv_tasks/config/config_pptsm_v2_worker.hpp
+++ generic_cv_tasks/config/config_pptsm_v2_worker.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "config_action_classify_model_select.hpp"
+#include "config_alert_sender.hpp"
 #include "config_select_action_to_classify.hpp"
 
 struct config_ppstm_v2_worker
 {
     config_action_classify_model_select config_action_classify_model_select_;
+    config_alert_sender config_alert_sender_;
     config_select_action_to_classify config_select_action_to_classify_;
 };
