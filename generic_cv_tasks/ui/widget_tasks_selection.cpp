2745d6e2b3a6b457a1de3da932fe961a85e56817
stereomatchingkiss <thamngapwei@gmail.com>
2023-12-20 15:13:17 +0800

    first commit

---
+++ generic_cv_tasks/ui/widget_tasks_selection.cpp
@@ -0,0 +1,65 @@
+#include "widget_tasks_selection.hpp"
+#include "ui_widget_tasks_selection.h"
+
+#include "../config/config_tasks_selection.hpp"
+
+#include <QJsonObject>
+
+namespace{
+
+QString const state_mode("state_mode");
+
+}
+
+widget_tasks_selection::widget_tasks_selection(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::widget_tasks_selection)
+{
+    ui->setupUi(this);
+
+    ui->comboBoxTasks->addItem(tr("Object tracking"));
+    ui->comboBoxTasks->addItem(tr("Action classify"));
+}
+
+widget_tasks_selection::~widget_tasks_selection()
+{
+    delete ui;
+}
+
+config_tasks_selection widget_tasks_selection::get_config() const noexcept
+{
+    config_tasks_selection resutls;
+    switch(ui->comboBoxTasks->currentIndex()){
+    case 0:{
+        resutls.task_ = enum_config_tasks::object_tracking;
+        break;
+    }
+    case 1:{
+        resutls.task_ = enum_config_tasks::action_classify;
+        break;
+    }
+    default:
+        break;
+    }
+
+    return resutls;
+}
+
+QJsonObject widget_tasks_selection::get_states() const
+{
+    QJsonObject obj;
+    obj[state_mode] = ui->comboBoxTasks->currentText();
+
+    return obj;
+}
+
+void widget_tasks_selection::set_states(const QJsonObject &val)
+{
+    if(val.contains(state_mode)){
+        if(val[state_mode].toString() == "Object tracking"){
+            ui->comboBoxTasks->setCurrentIndex(0);
+        }else{
+            ui->comboBoxTasks->setCurrentIndex(1);
+        }
+    }
+}
---
+++ generic_cv_tasks/ui/widget_tasks_selection.hpp
@@ -0,0 +1,28 @@
+#ifndef WIDGET_TASKS_SELECTION_HPP
+#define WIDGET_TASKS_SELECTION_HPP
+
+#include <QWidget>
+
+namespace Ui {
+class widget_tasks_selection;
+}
+
+struct config_tasks_selection;
+
+class widget_tasks_selection : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit widget_tasks_selection(QWidget *parent = nullptr);
+    ~widget_tasks_selection();
+
+    config_tasks_selection get_config() const noexcept;
+    QJsonObject get_states() const;
+    void set_states(QJsonObject const &val);
+
+private:
+    Ui::widget_tasks_selection *ui;
+};
+
+#endif // WIDGET_TASKS_SELECTION_HPP
