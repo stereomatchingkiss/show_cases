2839352dc40b8d2fcb0aa255e0833965ab08a726
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-05 17:22:15 +0800

    test paddel ocr v4 with opencv dnn

--- small_tests/opencv_paddle_ocr_test/main.cpp
+++ small_tests/opencv_paddle_ocr_test/main.cpp
@@ -1,7 +1,9 @@
 #include <cv_algo/ocr/paddle_ocr/paddle_ocr_det_opencv.hpp>
 #include <cv_algo/ocr/paddle_ocr/paddle_ocr_rec_onnx.hpp>
+#include <cv_algo/ocr/paddle_ocr/paddle_ocr_rec_opencv.hpp>
 
 #include <opencv2/core.hpp>
+#include <opencv2/dnn.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
@@ -36,7 +38,8 @@ void show_text_det_result(paddle_ocr_det_opencv &det, std::string const &im_name
     Visualization(input, det_out);
 }
 
-void detect_text(std::string const &im_name, paddle_ocr_det_opencv &det, paddle_ocr_rec_onnx &rec)
+template<typename T>
+void detect_text(std::string const &im_name, paddle_ocr_det_opencv &det, T &rec)
 {
     auto input = cv::imread(im_name);
     auto det_out = det.predict(input);
@@ -51,7 +54,9 @@ void detect_text(std::string const &im_name, paddle_ocr_det_opencv &det, paddle_
 int main(int argc, char *argv[])
 {    
     paddle_ocr_det_opencv det("ch_PP-OCRv4_det_simple.onnx");
-    paddle_ocr_rec_onnx rec("ch_PP-OCRv4_rec_infer.onnx", "ppocr_keys_v1.txt");
+    //paddle_ocr_rec_onnx rec("ch_PP-OCRv4_rec_infer.onnx", "ppocr_keys_v1.txt");
+
+    paddle_ocr_rec_opencv rec("ch_PP-OCRv4_rec_simple.onnx", "ppocr_keys_v1.txt");
 
     //resize_w = 498
     //resize_w = 375
@@ -59,9 +64,11 @@ int main(int argc, char *argv[])
     //resize_w = 375
     //resize_w = 85
     //resize_w = 55
-    //detect_text("english-poetry-100696.png", det, rec);
+    detect_text("english-poetry-100696.png", det, rec);
 
-    detect_text("english.png", det, rec);
+    //detect_text("english.png", det, rec);
+
+    //cv::dnn::readNet("ch_PP-OCRv4_rec_simple.onnx");
 
     /*for(size_t i = 0; i != 10; ++i){
         detect_text("english-poetry-100696.png", det, rec);
