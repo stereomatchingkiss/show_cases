37a40834805e7b8ad3875dded625986e944aff50
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-10 00:44:08 +0800

    separate pen settings for text, easier to read

--- anpr_tasks/algo/anpr_worker.cpp
+++ anpr_tasks/algo/anpr_worker.cpp
@@ -6,6 +6,8 @@
 
 #include <cv_algo/converter/qt_and_cv_rect_converter.hpp>
 
+#include <QDebug>
+
 #include <QPainter>
 #include <QPen>
 
@@ -16,15 +18,15 @@ struct anpr_worker::impl
     impl(config_anpr_worker const &config) :
         algo_{config.config_object_detect_model_select_}
     {
-        pen_plate_.setColor(Qt::blue);
-        pen_plate_.setWidth(5);
-
+        pen_plate_.setColor(Qt::darkRed);
+        pen_text_.setColor(Qt::yellow);
+        pen_text_.setWidth(10);
         pen_vechile_.setColor(Qt::green);
     }
 
     void predict(QImage &img)
     {
-        auto const pre_results = algo_.predict(img);
+        auto const pre_results = algo_.predict(img);        
         QPainter painter(&img);
         for(auto const &val : pre_results){
             painter.setPen(pen_vechile_);
@@ -32,6 +34,7 @@ struct anpr_worker::impl
             if(!val.plate_rect_.empty()){
                 painter.setPen(pen_plate_);
                 painter.drawRect(convert_cv_rect_to_qrect(val.plate_rect_));
+                painter.setPen(pen_text_);
                 painter.drawText(QPoint(val.plate_rect_.x, val.plate_rect_.y - 5), val.plate_num_);
             }
         }
@@ -40,6 +43,7 @@ struct anpr_worker::impl
     anpr_algo algo_;
     config_anpr_worker config_;
     QPen pen_plate_;
+    QPen pen_text_;
     QPen pen_vechile_;
 };
 
@@ -56,7 +60,7 @@ anpr_worker::~anpr_worker()
 }
 
 void anpr_worker::process_results(std::any frame)
-{
+{    
     auto qimg = std::any_cast<QImage>(frame);
     impl_->predict(qimg);
 
