eeefbe70a0d1d27c68806ac6e1c4ad76b002969e
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-10 00:27:20 +0800

    support new type

--- libs/utils/qimage_to_cvmat.cpp
+++ libs/utils/qimage_to_cvmat.cpp
@@ -7,6 +7,12 @@ namespace flt{
 std::pair<cv::Mat, bool> qimg_convert_to_cvmat_non_copy(QImage &qimg, bool gray_to_color)
 {
     switch(qimg.format()){
+    case QImage::Format_RGBA64:
+    case QImage::Format_RGBA64_Premultiplied:{
+        cv::Mat mat = cv::Mat(qimg.height(), qimg.width(), CV_16UC4, qimg.bits(), qimg.bytesPerLine());
+        cv::cvtColor(mat, mat, cv::COLOR_RGBA2RGB);
+        return {mat, false};
+    }
     case QImage::Format_ARGB32:
     case QImage::Format_RGB32:
     case QImage::Format_RGBA8888_Premultiplied:
