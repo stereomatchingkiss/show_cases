2abafa5b3f97c0b016afe7fce62af78f3ed9476d
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-18 23:23:00 +0800

    first commit

---
+++ aruco_tasks/ui/widget_source_selection.cpp
@@ -0,0 +1,96 @@
+#include "widget_source_selection.hpp"
+#include "ui_widget_source_selection.h"
+
+#include "../config/config_source_selection.hpp"
+
+#include <multimedia/network/frame_capture_websocket_params.hpp>
+#include <multimedia/stream_enum.hpp>
+
+#include <QJsonObject>
+
+namespace{
+
+QString const state_source_type("state_source_type");
+QString const state_websocket_url("state_websocket_url");
+
+}
+
+using namespace flt::mm;
+
+widget_source_selection::widget_source_selection(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::widget_source_selection)
+{
+    ui->setupUi(this);
+}
+
+widget_source_selection::~widget_source_selection()
+{
+    delete ui;
+}
+
+config_source_selection widget_source_selection::get_config() const
+{
+    using stype = stream_source_type;
+
+    config_source_selection config;
+    if(ui->radioButtonImage->isChecked()){
+        config.source_type_ = stype::image;
+    }else if(ui->radioButtonWebSockets->isChecked()){
+        config.source_type_ = stype::websocket;
+    }
+
+    config.websocket_url_ = ui->lineEditWebSockets->text();
+
+    return config;
+}
+
+QJsonObject widget_source_selection::get_states() const
+{
+    using stype = stream_source_type;
+
+    QJsonObject states;
+    if(ui->radioButtonImage->isChecked()){
+        states[state_source_type] = static_cast<int>(stype::image);
+    }else if(ui->radioButtonWebSockets->isChecked()){
+        states[state_source_type] = static_cast<int>(stype::websocket);
+    }
+
+    states[state_websocket_url] = ui->lineEditWebSockets->text();
+
+
+    return states;
+}
+
+frame_capture_websocket_params widget_source_selection::get_frame_capture_websocket_params() const
+{
+    frame_capture_websocket_params params;
+    params.url_ = ui->lineEditWebSockets->text();
+
+    return params;
+}
+
+void widget_source_selection::set_states(QJsonObject const &val)
+{
+    if(val.contains(state_source_type)){
+        using stype = stream_source_type;
+        switch(static_cast<stype>(val[state_source_type].toInt())){
+        case stype::image:{
+            ui->radioButtonImage->setChecked(true);
+            break;
+        }
+        case stype::websocket:{
+            ui->radioButtonWebSockets->setChecked(true);
+            break;
+        }
+        default:{
+            ui->radioButtonWebSockets->setChecked(true);
+            break;
+        }
+        }
+    }
+
+    if(val.contains(state_websocket_url)){
+        ui->lineEditWebSockets->setText(val[state_websocket_url].toString());
+    }
+}
