e7af63d6c63fd2fa3f31ce72b63c5c7efbe5568f
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-19 23:34:52 +0800

    implement aruco detect for image

--- aruco_tasks/ui/widget_stacks_aruco_detector.cpp
+++ aruco_tasks/ui/widget_stacks_aruco_detector.cpp
@@ -1,7 +1,25 @@
 #include "widget_stacks_aruco_detector.hpp"
 #include "ui_widget_stacks_aruco_detector.h"
 
+#include "../algo/aruco_detect_worker.hpp"
+
+#include "../config/config_aruco_detect_worker.hpp"
+
 #include "dialog_aruco_detector_params.hpp"
+#include "widget_image_player.hpp"
+#include "widget_source_selection.hpp"
+
+#include <multimedia/camera/frame_capture_qcamera.hpp>
+#include <multimedia/camera/frame_capture_qcamera_params.hpp>
+#include <multimedia/camera/frame_capture_qmediaplayer.hpp>
+#include <multimedia/camera/frame_capture_qmediaplayer_params.hpp>
+#include <multimedia/camera/frame_process_controller.hpp>
+#include <multimedia/network/frame_capture_websocket.hpp>
+#include <multimedia/network/frame_capture_websocket_params.hpp>
+#include <multimedia/sound/alert_sound_manager.hpp>
+#include <multimedia/stream_enum.hpp>
+
+using namespace flt::mm;
 
 widget_stacks_aruco_detector::widget_stacks_aruco_detector(QWidget *parent) :
     QWidget(parent),
@@ -20,14 +38,89 @@ widget_stacks_aruco_detector::~widget_stacks_aruco_detector()
 void widget_stacks_aruco_detector::init_stacked_widget()
 {
     dialog_aruco_detector_params_ = new dialog_aruco_detector_params;
+    widget_image_player_ = new widget_image_player;
+    widget_source_selection_ = new widget_source_selection;
 
     ui->stackedWidget->addWidget(dialog_aruco_detector_params_);
+    ui->stackedWidget->addWidget(widget_source_selection_);
+    ui->stackedWidget->addWidget(widget_image_player_);
 
-    ui->stackedWidget->setCurrentWidget(dialog_aruco_detector_params_);
+    ui->stackedWidget->setCurrentWidget(widget_source_selection_);
+}
+
+void widget_stacks_aruco_detector::create_frame_capture()
+{
+    if(widget_source_selection_->get_source_type() == stream_source_type::websocket){
+        sfwmw_ = std::make_unique<frame_capture_websocket>(widget_source_selection_->get_frame_capture_websocket_params());
+
+        connect(process_controller_.get(), &frame_process_controller::send_process_results,
+                widget_image_player_, &widget_image_player::display_frame);
+    }else{
+        sfwmw_ = nullptr;
+        connect(widget_image_player_, &widget_image_player::image_selected,
+                process_controller_.get(), &frame_process_controller::predict);
+        connect(process_controller_.get(), &frame_process_controller::send_process_results,
+                this, &widget_stacks_aruco_detector::display_frame);
+        emit process_controller_->start();
+    }
+}
+
+void widget_stacks_aruco_detector::display_frame(std::any input)
+{
+    widget_image_player_->display_frame(std::move(input));
+}
+
+void widget_stacks_aruco_detector::next_page_is_widget_image_player()
+{
+    ui->stackedWidget->setCurrentWidget(widget_image_player_);
+    ui->pushButtonNext->setVisible(false);
+
+    config_aruco_detect_worker config = dialog_aruco_detector_params_->get_config();
+    auto worker = new aruco_detect_worker(std::move(config));
+
+    if(widget_source_selection_->get_source_type() == stream_source_type::websocket){
+        sfwmw_ = std::make_unique<frame_capture_websocket>(widget_source_selection_->get_frame_capture_websocket_params());
+        process_controller_ = std::make_shared<frame_process_controller>(worker);
+        connect(process_controller_.get(), &frame_process_controller::send_process_results,
+                widget_image_player_, &widget_image_player::display_frame);
+
+        emit process_controller_->start();
+        sfwmw_->add_listener(process_controller_, this);
+        sfwmw_->start();
+    }else{
+        sfwmw_ = nullptr;
+        process_controller_ = std::make_shared<frame_process_controller>(worker);
+        connect(widget_image_player_, &widget_image_player::image_selected,
+                process_controller_.get(), &frame_process_controller::predict);
+        connect(process_controller_.get(), &frame_process_controller::send_process_results,
+                this, &widget_stacks_aruco_detector::display_frame);
+
+        emit process_controller_->start();
+    }
 }
 
 void widget_stacks_aruco_detector::on_pushButtonPrev_clicked()
 {
-    emit prev_button_clicked();
+    ui->pushButtonNext->setVisible(true);
+    if(ui->stackedWidget->currentWidget() == widget_image_player_){
+        ui->stackedWidget->setCurrentWidget(dialog_aruco_detector_params_);
+    }else if(ui->stackedWidget->currentWidget() == dialog_aruco_detector_params_){
+        ui->stackedWidget->setCurrentWidget(widget_source_selection_);
+    }else{
+        emit prev_button_clicked();
+    }
+}
+
+
+void widget_stacks_aruco_detector::on_pushButtonNext_clicked()
+{
+    ui->pushButtonPrev->setVisible(true);
+
+    if(ui->stackedWidget->currentWidget() == widget_source_selection_){
+        ui->stackedWidget->setCurrentWidget(dialog_aruco_detector_params_);
+    }else if(ui->stackedWidget->currentWidget() == dialog_aruco_detector_params_){
+        ui->pushButtonNext->setVisible(false);
+        next_page_is_widget_image_player();
+    }
 }
 
--- aruco_tasks/ui/widget_stacks_aruco_detector.hpp
+++ aruco_tasks/ui/widget_stacks_aruco_detector.hpp
@@ -3,11 +3,22 @@
 
 #include <QWidget>
 
+#include <any>
+
 namespace Ui {
 class widget_stacks_aruco_detector;
 }
 
 class dialog_aruco_detector_params;
+class widget_image_player;
+class widget_source_selection;
+
+namespace flt::mm{
+
+class frame_process_controller;
+class single_frame_with_multi_worker_base;
+
+}
 
 class widget_stacks_aruco_detector : public QWidget
 {
@@ -23,12 +34,22 @@ signals:
 private slots:
     void on_pushButtonPrev_clicked();
 
+    void on_pushButtonNext_clicked();
+
 private:
+    void create_frame_capture();
+    void display_frame(std::any input);
     void init_stacked_widget();
+    void next_page_is_widget_image_player();
 
     Ui::widget_stacks_aruco_detector *ui;
 
     dialog_aruco_detector_params *dialog_aruco_detector_params_;
+    widget_image_player *widget_image_player_;
+    widget_source_selection *widget_source_selection_;
+
+    std::shared_ptr<flt::mm::frame_process_controller> process_controller_;
+    std::unique_ptr<flt::mm::single_frame_with_multi_worker_base> sfwmw_;
 };
 
 #endif // WIDGET_STACKS_ARUCO_DETECTOR_HPP
