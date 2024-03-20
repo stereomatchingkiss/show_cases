c69b0c14eab2743b8db4819bc9bc5ed44a83bdab
stereomatchingkiss <thamngapwei@gmail.com>
2024-03-21 02:28:01 +0800

    add new functions

--- pose_tasks/algo/pose_estimation_common_func.hpp
+++ pose_tasks/algo/pose_estimation_common_func.hpp
@@ -8,23 +8,44 @@
 
 #include <concepts>
 
-template<typename T, typename U>
-requires std::is_class_v<T> && std::is_class_v<U>
-T predict_pose(U &inout, float confidence, flt::cvt::pose::movenet_single_pose_estimate &net)
+template<typename T>
+requires std::is_class_v<T>
+T predict_pose(QImage &qimg, float confidence, flt::cvt::pose::movenet_single_pose_estimate &net)
 {
-    auto [mat, non_copy] = flt::qimg_convert_to_cvmat_non_copy(inout.qimg_);
+    auto [mat, non_copy] = flt::qimg_convert_to_cvmat_non_copy(qimg);
     cv::cvtColor(mat, mat, cv::COLOR_BGR2RGB);
 
-    T results;
-    results.is_target_ = inout.is_target_;
+    T results;    
     results.points_ = net.predict(mat);
     flt::cvt::pose::draw(mat, results.points_, confidence);
     if(non_copy){
-        results.qimg_ = inout.qimg_;
+        results.qimg_ = qimg;
     }else{
         //Todo : reduce useless operations
         results.qimg_ = QImage(mat.data, mat.cols, mat.rows, mat.step[0], QImage::Format_RGB888).copy();
     }
 
     return results;
+}
+
+inline
+std::string pose_model_root()
+{
+#ifndef WASM_BUILD
+    return "assets/";
+#else
+    return "";
+#endif
+}
+
+inline
+std::string pose_bin_path()
+{
+    return pose_model_root() + "thunder.bin";
+}
+
+inline
+std::string pose_param_path()
+{
+    return pose_model_root() + "thunder.param";
 }
