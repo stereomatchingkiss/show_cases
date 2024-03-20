36a4d83e89bc51e141a55c9e4664893972e29a97
stereomatchingkiss <thamngapwei@gmail.com>
2024-03-20 21:07:58 +0800

    remove useless codes

--- pose_tasks/algo/estimate_pose_similarity_worker_input.hpp
+++ pose_tasks/algo/estimate_pose_similarity_worker_input.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 #include <QImage>
+#include <QString>
 
 struct estimate_pose_similarity_worker_input
 {
     bool is_target_ = false;
     QImage qimg_;
-
-    QString json_contents_;
 };
