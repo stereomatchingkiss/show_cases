fa500678696d4fea4ad44cd5dce14434936eb88f
stereomatchingkiss <thamngapwei@gmail.com>
2024-07-09 17:34:48 +0800

    able to check width/height ratio of person

--- generic_cv_tasks/algo/fall_down_detect/fall_down_obj_det_worker.cpp
+++ generic_cv_tasks/algo/fall_down_detect/fall_down_obj_det_worker.cpp
@@ -4,6 +4,8 @@
 #include "../../config/config_fall_down_obj_det_worker.hpp"
 #include "../../config/config_generic_obj_detector.hpp"
 
+#include "../../global/global_keywords.hpp"
+
 #include "../generic_obj_detector.hpp"
 #include "../generic_worker_results.hpp"
 
@@ -13,7 +15,6 @@
 #include <cv_algo/obj_detect/obj_det_utils.hpp>
 #include <cv_algo/obj_detect/nanodet/nanodet.hpp>
 
-#include <cv_algo/tracker/track_object_pass.hpp>
 #include <cv_algo/tracker/track_results.hpp>
 #include <cv_algo/tracker/byte_track/BYTETracker.hpp>
 
@@ -32,15 +33,23 @@
 
 using namespace flt;
 using namespace flt::cvt;
+using namespace flt::cvt::det;
 using namespace flt::cvt::tracker;
 
 struct fall_down_obj_det_worker::impl
 {
     impl(config_fall_down_obj_det_worker const &config) :
-        config_{config}
+        config_{config},
+        names_{global_keywords().coco_names()},
+        tracker_{30,
+                 30,
+                 config_.config_object_detect_model_select_.confidence_,
+                 std::min(config_.config_object_detect_model_select_.confidence_ + 0.05f, 0.8f),
+                   0.8f}
     {
         config_generic_obj_detector gconfig;
-        gconfig.config_object_detect_model_select_ = config_.config_object_detect_model_select_;        
+        gconfig.config_object_detect_model_select_ = config_.config_object_detect_model_select_;
+        gconfig.config_select_object_to_detect_.selected_object_.insert("person");
         obj_det_ = std::make_unique<generic_obj_detector>(std::move(gconfig));
     }
 
@@ -58,41 +67,26 @@ struct fall_down_obj_det_worker::impl
         }//*/
 
         return alarm_on;
-    }
-
-    void clear_written_id()
-    {
-        if(im_ids_ % 500 == 0){
-            for(auto it = std::begin(written_id_); it != std::end(written_id_);){
-                if(track_obj_pass_->id_is_under_track(*it)){
-                    written_id_.erase(it++);
-                }else{
-                    ++it;
-                }
-            }
-        }
-    }
+    }    
 
     void change_alert_sender_config(const config_alert_sender &val)
     {
         config_.config_alert_sender_ = val;
-    }    
+    }        
 
-    void draw_pass_results(cv::Mat &mat, track_results const &pass_results) const
+    float width_height_ratio(box_info const &val) const noexcept
     {
-        cv::putText(mat, std::format("up:{}, down:{}", pass_results.count_top_pass_, pass_results.count_bottom_pass_),
-                    cv::Point(0, 50), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(200, 255, 0), 4);
-        cv::putText(mat, std::format("left:{}, right:{}", pass_results.count_left_pass_, pass_results.count_right_pass_),
-                    cv::Point(0, 100), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(200, 255, 0), 4);
-        cv::putText(mat, std::format("in the rect:{}", pass_results.count_in_center_),
-                    cv::Point(0, 150), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(200, 255, 0), 4);
-        if(!scaled_roi_.empty()){
-            flt::cvt::utils::draw_empty_rect(mat, scaled_roi_);
-        }
-    }    
+        auto const tl = val.tl();
+        auto const br = val.br();
+        //qDebug()<<std::format("{}:{},{}:{}", tl.x, tl.y, br.x, br.y).c_str();
+        auto const width = br.x - tl.x;
+        auto const height = br.y - tl.y;
+
+        return std::abs(width/(height + 0.001f));
+    }
 
     auto track_obj(cv::Mat &mat)
-    {
+    {        
         auto det_results = obj_det_->predict(mat);
         auto track_obj = box_info_to_byte_track_obj(det_results);
         auto const track_ptr_vec = tracker_.update(track_obj);
@@ -100,21 +94,24 @@ struct fall_down_obj_det_worker::impl
         det_results = byte_track_obj_to_box_info(track_ptr_vec);
         for(auto const &val : det_results){
             det::draw_bboxes_custom(mat, val, std::format("{}:{}", names_[val.label_], val.track_id_));
+            auto const wh_ratio = width_height_ratio(val);
+            qDebug()<<"wh ratio = "<<wh_ratio<<", "<<config_.width_height_ratio_;
+            if(wh_ratio >= config_.width_height_ratio_){
+                det::draw_bboxes_custom(mat, val, std::format("{}:{}:fall", names_[val.label_], val.track_id_));
+            }else{
+                det::draw_bboxes_custom(mat, val, std::format("{}:{}", names_[val.label_], val.track_id_));
+            }
         }
 
         return det_results;
     }
 
 
-    config_fall_down_obj_det_worker config_;
-    size_t im_ids_ = 0;
+    config_fall_down_obj_det_worker config_;    
     std::vector<std::string> names_;
-    std::unique_ptr<generic_obj_detector> obj_det_;
-    std::vector<bool> obj_to_detect_;
+    std::unique_ptr<generic_obj_detector> obj_det_;    
     cv::Rect scaled_roi_;
     BYTETracker tracker_;
-    std::unique_ptr<track_object_pass> track_obj_pass_;
-    std::set<int> written_id_;
 };
 
 fall_down_obj_det_worker::fall_down_obj_det_worker(const config_fall_down_obj_det_worker &config, QObject *parent) :
@@ -139,29 +136,15 @@ void fall_down_obj_det_worker::process_results(std::any frame)
     auto qimg = std::any_cast<QImage>(frame).convertToFormat(QImage::Format_RGB888);
     auto mat = cv::Mat(qimg.height(), qimg.width(), CV_8UC3, qimg.bits(), qimg.bytesPerLine());
 
-    if(!impl_->track_obj_pass_ && impl_->config_.roi_.isValid()){
-        impl_->scaled_roi_ = convert_qrectf_to_cv_rect(impl_->config_.roi_, mat.cols, mat.rows);
-        impl_->track_obj_pass_ =
-            std::make_unique<cvt::tracker::track_object_pass>(impl_->scaled_roi_, 30);
-    }
-
     auto const det_results = impl_->track_obj(mat);
-    auto const pass_results = impl_->track_obj_pass_->track(det_results);
-    impl_->draw_pass_results(mat, pass_results);//*/
-
     generic_worker_results results;
-    /*if(impl_->check_alarm_condition(pass_results, qimg)){
-        results.alarm_on_ = true;
-        ++impl_->im_ids_;
-        impl_->clear_written_id();
-        if(impl_->alert_save_.send_alert()){
-            if(impl_->alert_save_.send_by_text()){
-                emit send_alert_by_text(impl_->alert_save_.get_alert_info());
-            }else{
-                emit send_alert_by_binary(impl_->alert_save_.get_alert_info());
-            }
+
+    if(impl_->config_.roi_.isValid()){
+        impl_->scaled_roi_ = convert_qrectf_to_cv_rect(impl_->config_.roi_, mat.cols, mat.rows);
+        if(!impl_->scaled_roi_.empty()){
+            flt::cvt::utils::draw_empty_rect(mat, impl_->scaled_roi_);
         }
-    }//*/
+    }
 
     //do not move it, since in the future this algo may need to support multi-stream
     results.mat_ = qimg;
