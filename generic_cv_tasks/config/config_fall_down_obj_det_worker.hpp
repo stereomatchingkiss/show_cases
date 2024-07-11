e9394f834120f445da7cc44f44ba94eb2060dc99
stereomatchingkiss <thamngapwei@gmail.com>
2024-07-11 18:40:43 +0800

    add new config

--- generic_cv_tasks/config/config_fall_down_obj_det_worker.hpp
+++ generic_cv_tasks/config/config_fall_down_obj_det_worker.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "config_alert_sender.hpp"
+#include "config_fall_down_obj_det_alert.hpp"
 #include "config_fall_down_condition.hpp"
 #include "config_object_detect_model_select.hpp"
 
@@ -10,7 +11,8 @@ struct config_fall_down_obj_det_worker
 {
     config_alert_sender config_alert_sender_;
     config_fall_down_condition config_fall_down_condition_;
+    config_fall_down_obj_det_alert config_fall_down_obj_det_alert_;
     config_object_detect_model_select config_object_detect_model_select_;
 
-    QRectF roi_;    
+    QRectF roi_;
 };
