f2477d41871f8ba43029b613d257325dc01f5f3f
stereomatchingkiss <thamngapwei@gmail.com>
2024-08-08 16:47:45 +0800

    first commit

---
+++ libs/network/simple_email_sender.cpp
@@ -0,0 +1,60 @@
+#include "simple_email_sender.hpp"
+
+using namespace SimpleMail;
+
+namespace flt::net{
+
+simple_email_sender::simple_email_sender(QString const &email_address,
+                                         QString const &password,
+                                         QString const &sender_name,
+                                         QString const &host,
+                                         quint16 port) :
+    sender_{email_address, sender_name}
+{
+    server_.setHost(host);
+    server_.setPort(port);
+    server_.setConnectionType(Server::SslConnection);
+
+    server_.setUsername(email_address.first(email_address.indexOf("@")));
+    server_.setPassword(password);
+}
+
+simple_email_sender::~simple_email_sender()
+{
+
+}
+
+void simple_email_sender::add_send_to(QString const &email_address, QString const &recipient)
+{
+    if(auto it = send_to_list_.find(email_address); it == std::end(send_to_list_)){
+        send_to_list_.emplace(email_address, recipient);
+    }else{
+        it->second = recipient;
+    }
+}
+
+void simple_email_sender::remove_send_to(QString const &email_address)
+{
+    if(auto it = send_to_list_.find(email_address); it != std::end(send_to_list_)){
+        send_to_list_.erase(it);
+    }
+}
+
+SimpleMail::ServerReply *simple_email_sender::send(std::vector<std::shared_ptr<SimpleMail::MimePart>> const &parts,
+                                                   QString const &subject)
+{
+    MimeMessage message;
+    message.setSender(sender_);
+    message.setSubject(subject);
+
+    for(auto const &val : send_to_list_){
+        message.addTo(EmailAddress(val.first, val.second));
+    }
+    for(auto const &val : parts){
+        message.addPart(val);
+    }
+
+    return server_.sendMail(message);
+}
+
+}
---
+++ libs/network/simple_email_sender.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <SimpleMail>
+
+#include <QString>
+
+#include <map>
+
+namespace flt::net{
+
+class simple_email_sender
+{    
+public:
+    explicit simple_email_sender(QString const &email_address,
+                                 QString const &password,
+                                 QString const &sender_name,
+                                 QString const &host = "smtp.gmail.com",
+                                 quint16 port = 465);
+    ~simple_email_sender();
+
+    void add_send_to(QString const &email_address, QString const &recipient);
+
+    void remove_send_to(QString const &email_address);
+
+    SimpleMail::ServerReply* send(std::vector<std::shared_ptr<SimpleMail::MimePart>> const &parts,
+                                  QString const &subject = "Send by GenericCVTask");
+
+private:
+    SimpleMail::EmailAddress sender_;
+    std::map<QString, QString> send_to_list_;
+    SimpleMail::Server server_;
+};
+
+}
