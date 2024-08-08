cce559c3eb1a6e5e40a102a4c8790caca5171598
stereomatchingkiss <thamngapwei@gmail.com>
2024-08-08 21:31:45 +0800

    add parameter of email

--- generic_cv_tasks/config/config_alert_sender.hpp
+++ generic_cv_tasks/config/config_alert_sender.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "config_dialog_email_list.hpp"
+
+#include <QStringList>
 #include <QUrl>
 
 struct config_alert_sender
@@ -7,5 +10,10 @@ struct config_alert_sender
     bool activate_ = false;
     bool save_reports_ = true;
     bool send_by_text_ = true;
-    QUrl url_;    
+    QUrl url_;
+
+    bool email_alert_on_ = false;
+    QString email_address_;
+    QString email_password_;
+    config_dialog_email_list config_dialog_email_list_;
 };
